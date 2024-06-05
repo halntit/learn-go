844aae82f14c566d10aa9a23fab443414a860991
Ha Le <halntit@gmail.com>
2024-06-05 09:05:37 +0700

    run postgres, install migrate CLI

--- bank-ex/README.md
+++ bank-ex/README.md
@@ -4,3 +4,9 @@
 
 ## install PostgreSQL
 - ```docker pull postgres:16-alpine```
+- ```docker run --name postgres16 -e POSTGRES_USER=root POSTGRES_PASSWORD=secret -p 5432:5432 -d postgres:16-alpine```
+
+## Go Migrate CLI
+- source: https://github.com/golang-migrate/migrate/tree/master/cmd/migrate
+- ```curl -L https://github.com/golang-migrate/migrate/releases/download/v4.17.1/migrate.linux-386.tar.gz | tar xvz```
+- ```mv migrate $GOPATH/bin/migrate```
--- bank-ex/db/simple-bank-postgres.sql
+++ bank-ex/db/simple-bank-postgres.sql
@@ -8,15 +8,15 @@ CREATE TABLE "accounts" (
 
 CREATE TABLE "entities" (
   "id" bigserial PRIMARY KEY,
-  "account_id" bigint,
+  "account_id" bigint NOT NULL,
   "amount" bigint NOT NULL,
   "created_at" timestampz NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE "transfers" (
   "id" bigserial PRIMARY KEY,
-  "from_acount_id" bigint,
-  "to_acount_id" bigint,
+  "from_acount_id" bigint NOT NULL,
+  "to_acount_id" bigint NOT NULL,
   "amount" bigint NOT NULL,
   "created_at" timestampz NOT NULL DEFAULT (now())
 );
